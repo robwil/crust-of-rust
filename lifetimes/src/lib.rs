44e1b156e7f1c817e00b2add2aaac5f06cd7940a
Rob Williams <rwilliams@spotify.com>
2021-01-23 14:05:18 -0500

    multiple lifetimes strsplit library

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,54 @@
+#![warn(rust_2018_idioms)]
+
+pub struct StrSplit<'haystack, 'delimiter> {
+    remainder: Option<&'haystack str>,
+    delimiter: &'delimiter str,
+}
+
+impl<'haystack, 'delimiter> StrSplit<'haystack, 'delimiter> {
+    pub fn new(haystack: &'haystack str, delimiter: &'delimiter str) -> Self {
+        Self {
+            remainder: Some(haystack),
+            delimiter,
+        }
+    }
+}
+
+impl<'haystack> Iterator for StrSplit<'haystack, '_> {
+    type Item = &'haystack str;
+    fn next(&mut self) -> Option<Self::Item> {
+        let remainder = self.remainder.as_mut()?;
+        if let Some(next_delim) = remainder.find(self.delimiter) {
+            let until_delimiter = &remainder[..next_delim];
+            *remainder = &remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else {
+            self.remainder.take()
+        }
+    }
+}
+
+fn until_char(s: &str, c: char) -> &str {
+    StrSplit::new(s, &format!("{}", c))
+        .next()
+        .expect("StrSplit always give at least one result")
+}
+
+#[test]
+fn until_char_test() {
+    assert_eq!(until_char("hello world", 'o'), "hell");
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d ";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
+}
